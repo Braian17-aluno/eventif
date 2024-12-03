ab80357cd0d0327afb4292dfaeef01664a155522
unknown <braian.sergio@aluno.riogrande.ifrs.edu.br>
2024-12-02 21:30:45 -0300

    Speakers

--- contact/admin.py
+++ contact/admin.py
@@ -1,26 +1,26 @@
-from django.contrib import admin
-from contact.models import Contact
-from django.utils.timezone import now
-# Register your models here.
-class ContactModelAdmin(admin.ModelAdmin):
-  list_display = ('nome', 'telefone', 'email', 'mensagem','created_at')
-  date_hierarchy = 'created_at'
-  search_fields = ('nome', 'telefone', 'email', 'mensagem','created_at')
-  list_filter = ['created_at']
-  #actions = ['mark_as_paid',]
-  # def subscribed_today(self, obj):
-  #   return obj.created_at.date() == now().date()
-  # subscribed_today.short_description = 'Inscrito hoje?'
-  # subscribed_today.boolean = True
+# from django.contrib import admin
+# from contact.models import Contact
+# from django.utils.timezone import now
+# # Register your models here.
+# class ContactModelAdmin(admin.ModelAdmin):
+#   list_display = ('nome', 'telefone', 'email', 'mensagem','created_at')
+#   date_hierarchy = 'created_at'
+#   search_fields = ('nome', 'telefone', 'email', 'mensagem','created_at')
+#   list_filter = ['created_at']
+#   #actions = ['mark_as_paid',]
+#   # def subscribed_today(self, obj):
+#   #   return obj.created_at.date() == now().date()
+#   # subscribed_today.short_description = 'Inscrito hoje?'
+#   # subscribed_today.boolean = True
 
-  # def mark_as_paid(self, request, queryset):
-  #   count = queryset.update(paid=True)
-  #   if count == 1:
-  #     msg = "{} inscrição foi marcada como paga."
-  #   else:
-  #     msg = "{} inscrições foram marcadas como pagas"
-  #   self.message_user(request, msg.format(count))
-  # mark_as_paid.short_description = "Marcar como pago"
+#   # def mark_as_paid(self, request, queryset):
+#   #   count = queryset.update(paid=True)
+#   #   if count == 1:
+#   #     msg = "{} inscrição foi marcada como paga."
+#   #   else:
+#   #     msg = "{} inscrições foram marcadas como pagas"
+#   #   self.message_user(request, msg.format(count))
+#   # mark_as_paid.short_description = "Marcar como pago"
 
 
-admin.site.register(Contact, ContactModelAdmin)
+# admin.site.register(Contact, ContactModelAdmin)
--- contact/tests/test_model_contact.py
+++ contact/tests/test_model_contact.py
@@ -1,16 +1,16 @@
-from django.test import TestCase
-from datetime import datetime
-from contact.models import Contact
+# from django.test import TestCase
+# from datetime import datetime
+# from contact.models import Contact
 
-class ContactModelTest(TestCase):
-  def setUp(self):
-    self.obj = Contact(
-      nome = 'Braai',
-      telefone= '123981371904619',
-      email = 'braai@gmail.com',
-      mensagem = 'teste'
-    )
-    self.obj.save()
+# class ContactModelTest(TestCase):
+#   def setUp(self):
+#     self.obj = Contact(
+#       nome = 'Braai',
+#       telefone= '123981371904619',
+#       email = 'braai@gmail.com',
+#       mensagem = 'teste'
+#     )
+#     self.obj.save()
 
   # def test_create(self):
   #   self.assertTrue(Contact.objects.exists())
